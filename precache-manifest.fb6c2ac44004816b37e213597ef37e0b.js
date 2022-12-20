bd1ae55b5a2cfde0e0726e7ff7f66436e4ffba3d
AndrewMoskvinKyiv <moskvinavkyiv@gmail.com>
2022-12-20 09:22:08 +0300

    Updates

---
+++ precache-manifest.fb6c2ac44004816b37e213597ef37e0b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3eb4aa44f9fd1a5086ce78b0275463",
+    "url": "/Moskvin_portfolio/index.html"
+  },
+  {
+    "revision": "bfb36f1ca0fd40a6c976",
+    "url": "/Moskvin_portfolio/static/css/main.bb6a02f7.chunk.css"
+  },
+  {
+    "revision": "472fe636faa2ec616bea",
+    "url": "/Moskvin_portfolio/static/js/2.667bfa96.chunk.js"
+  },
+  {
+    "revision": "5e7a3bbd1766925a2d65977472adeaa7",
+    "url": "/Moskvin_portfolio/static/js/2.667bfa96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb36f1ca0fd40a6c976",
+    "url": "/Moskvin_portfolio/static/js/main.9a744450.chunk.js"
+  },
+  {
+    "revision": "69d60c611d92e71ac8d7",
+    "url": "/Moskvin_portfolio/static/js/runtime-main.bccd9503.js"
+  },
+  {
+    "revision": "d8c6a1ba58ae3c61e81c4a33d123b9b7",
+    "url": "/Moskvin_portfolio/static/media/ansys.d8c6a1ba.png"
+  },
+  {
+    "revision": "f643d5f30f7176caf56eefdbe3922c68",
+    "url": "/Moskvin_portfolio/static/media/carSharing.f643d5f3.png"
+  },
+  {
+    "revision": "8e1cba54b12171a2bf947e1adcbdff14",
+    "url": "/Moskvin_portfolio/static/media/cards.8e1cba54.png"
+  },
+  {
+    "revision": "cd0e5783f6880e8cb26f34b93d1dee91",
+    "url": "/Moskvin_portfolio/static/media/job3.cd0e5783.jpg"
+  },
+  {
+    "revision": "f4ee30201c682248b15d72052d08a1ce",
+    "url": "/Moskvin_portfolio/static/media/job4.f4ee3020.jpg"
+  },
+  {
+    "revision": "ed74366071acde71020c7ce358ca2208",
+    "url": "/Moskvin_portfolio/static/media/lay.ed743660.jpg"
+  },
+  {
+    "revision": "0c5d0a60f48097c95bfb9cb76a2179ac",
+    "url": "/Moskvin_portfolio/static/media/lay2.0c5d0a60.jpg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/Moskvin_portfolio/static/media/linkedin-in-brands.1e01f0c0.svg"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/Moskvin_portfolio/static/media/pngwing.com.70f87bba.png"
+  },
+  {
+    "revision": "8e60f7f28f1f09e38c881cb386f3917c",
+    "url": "/Moskvin_portfolio/static/media/sNetwork.8e60f7f2.png"
+  },
+  {
+    "revision": "bdc0fc45ab7bc8f910b9714e7b8f8bc0",
+    "url": "/Moskvin_portfolio/static/media/telegram-plane-brands.bdc0fc45.svg"
+  },
+  {
+    "revision": "493c500059d5c39503a504dbbe92fd81",
+    "url": "/Moskvin_portfolio/static/media/todolist.493c5000.png"
+  },
+  {
+    "revision": "923cb2223d722233d5abcbfb583b73e7",
+    "url": "/Moskvin_portfolio/static/media/vue.com.923cb222.png"
+  }
+]);
