85a35d72adffd5c96758ed4849192e873fc457cc
AndrewMoskvinKyiv <moskvinavkyiv@gmail.com>
2022-12-20 13:26:23 +0300

    rename_project

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 import telegram from "../../images/telegram-plane-brands.svg";
 import {StyledSocialNetworkImg} from "../my_works/example/example";
 import linkedIn from "../../images/linkedin-in-brands.svg";
+import phone from "../../images/phone-call.png";
+import email from "../../images/email.png";
+import location from "../../images/location.png";
 
 const FooterStyledInner = styled.div`
-padding-top: 30px;
+  padding-top: 30px;
 `
 const NetworkLinkStyled = styled.a`
-  cursor:pointer;
+  cursor: pointer;
   display: inline-block;
   transition: all 0.5s;
 `
@@ -28,28 +31,44 @@ const DivStyled = styled.div`
   flex-wrap: wrap;
 `
 const StyledP = styled.p`
- max-width: 500px;
+  max-width: 500px;
   width: 100%;
   font-size: 16px;
-  display:inline-block;
+  justify-content: center;
   color: #3e3e3e;
-  
+  display: flex;
+  align-items: center;
+`
+const StyledImg = styled.img`
+  margin-right: 10px;
+  height: 25px;
 `
 
 
 export const Footer = () => {
-    return(
+    return (
         <FooterStyledInner>
             <DivStyled>
-            <StyledP>tel:+905012728210(viber, telegram)</StyledP>
+                <StyledP>
+                    <StyledImg src={phone} alt='phone_icon'/> +905012728210(viber, telegram)
+                </StyledP>
 
-            <StyledP>email:andrewmoskvin1@gmail.com</StyledP>
+                <StyledP>
+                    <StyledImg src={email} alt='email_icon'/>andrewmoskvin1@gmail.com
+                </StyledP>
+
+                <StyledP>
+                    <StyledImg src={location} alt='location_icon'></StyledImg>
+                    Turkey/Antalya
+                </StyledP>
             </DivStyled>
             <NetworkLinkStyled className={'my_link'}>
 
-                <a href={"https://t.me/MoskvinAndrew"} target='_blank'><StyledSocialNetworkImg src={telegram} className={'my_picture'} /></a>
+                <a href={"https://t.me/MoskvinAndrew"} target='_blank'><StyledSocialNetworkImg src={telegram}
+                                                                                               className={'my_picture'}/></a>
                 {/*<><StyledSocialNetworkImg src={linkedIn} className={'my_picture'} /></>*/}
-                <a href={"https://www.linkedin.com/in/andrew-moskvin-0090011a5/"} target='_blank'><StyledSocialNetworkImg src={linkedIn} className={'my_picture'} /></a>
+                <a href={"https://www.linkedin.com/in/andrew-moskvin-0090011a5/"}
+                   target='_blank'><StyledSocialNetworkImg src={linkedIn} className={'my_picture'}/></a>
             </NetworkLinkStyled>
             <ParagraphStyled>Moskvin portfolio. 2021 ©</ParagraphStyled>
         </FooterStyledInner>
